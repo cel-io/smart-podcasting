1d94cca9d8f35bd1e4a6452f102f251fba8a72a9
Pedro Francisco Soares <1160714@isep.ipp.pt>
2022-06-13 20:00:49 +0100

    changes

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from config import SERVER
 
 if __name__ == "__main__":
 
-    sum = ServoMotorAgent(jid="servomotoragent" + SERVER, password="password")
-    future_sum = sum.start()
-    future_sum.result()
+    servomotor = ServoMotorAgent(jid="servomotoragent" + SERVER, password="password")
+    future_servomotor = servomotor.start()
+    future_servomotor.result()
 
-    calculator = PodcastManagerAgent(jid="podcastmanageragent" + SERVER, password="password")
-    future_calculator = calculator.start()
-    future_calculator.result()
+    podcastmanager = PodcastManagerAgent(jid="podcastmanageragent" + SERVER, password="password")
+    future_podcastmanager = podcastmanager.start()
+    future_podcastmanager.result()
 
-    client = MicrophoneAgent(op1=10.0, op2=20.0, jid="m1" + SERVER, password="password")
-    future_client = client.start()
-    future_client.result()
+    microphone = MicrophoneAgent(op1=10.0, op2=20.0, jid="m1" + SERVER, password="password")
+    future_microphone = microphone.start()
+    future_microphone.result()
 
     try:
-        while sum.is_alive() or calculator.is_alive() or client.is_alive():
+        while servomotor.is_alive() or podcastmanager.is_alive() or microphone.is_alive():
             time.sleep(1)
     except KeyboardInterrupt:
         print("Stopping...")
-    client.stop()
+    microphone.stop()
